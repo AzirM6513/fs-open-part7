53dca5973c0e7871c74403fa59f8fb2de3e5affe
azirm6513 <christianjortiz7+git@gmail.com>
2021-04-20 18:22:14 -0400

    move hooks to seperate file

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -1,50 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const useField = (type) => {
-  const [value, setValue] = useState('');
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  return {
-    type,
-    value,
-    onChange,
-  };
-};
-
-const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const { status, data } = await axios.get(baseUrl);
-
-      if (status === 200) {
-        setResources(data);
-      }
-    };
-
-    getData();
-  }, [baseUrl]);
-
-  const create = async (resource) => {
-    await axios.post(baseUrl, resource);
-    const { status, data } = await axios.get(baseUrl);
-
-    if (status === 200) {
-      setResources(data);
-    }
-  };
-
-  const service = {
-    create,
-  };
-
-  return [resources, service];
-};
+import React from 'react';
+import { useField, useResource } from './hooks/index';
 
 const App = () => {
   const content = useField('text');
