5cea8456dce0eb1761c457c52340b73909af8b9b
azirm6513 <christianjortiz7+git@gmail.com>
2021-04-20 17:37:51 -0400

    move Country component to seperate file

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -1,25 +1,7 @@
 import React, { useState } from 'react';
+
 import { useField, useCountry } from './hooks/index';
-
-const Country = ({ country }) => {
-  if (!country) {
-    return null;
-  }
-
-  if (!country.found) {
-    return <div>not found...</div>;
-  }
-
-  const { name, capital, population, flag } = country.country;
-  return (
-    <div>
-      <h3>{name} </h3>
-      <div>capital {capital} </div>
-      <div>population {population}</div>
-      <img src={flag} height='100' alt={`flag of ${name}`} />
-    </div>
-  );
-};
+import Country from './components/Country';
 
 const App = () => {
   const nameInput = useField('text');
